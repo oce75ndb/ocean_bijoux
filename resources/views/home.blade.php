9f79e85e96d4f86428ea1f051d4baa1bf15fa364
Océane Bondon <oceanebondon30@gmail.com>
2025-01-16 13:50:30 +0100

    update articles vedettes ok

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -18,7 +18,7 @@
             <!-- Produit 1 -->
             @if ($produitA)
                 <div class="bg-white shadow-md rounded-lg p-4 border border-gold">
-                    <img src="{{ asset('images/' . $produitA->image) }}" alt="{{ $produitA->nom }}" class="w-full h-48 object-cover rounded">
+                    <img src="{{ asset($produitA->image) }}" alt="{{ $produitA->nom }}" class="w-full h-48 object-cover rounded">
                     <h4 class="text-lg font-bold text-black mt-4">{{ $produitA->nom }}</h4>
                     <span class="block text-gold font-semibold mt-4">{{ number_format($produitA->prix, 2) }} €</span>
                     <a href="{{ route('produit.show', ['id' => $produitA->id]) }}" class="block mt-4 bg-black text-beige py-2 px-4 rounded text-center hover:bg-gold">Voir le produit</a>
@@ -28,7 +28,7 @@
             <!-- Produit 2 -->
             @if ($produitB)
                 <div class="bg-white shadow-md rounded-lg p-4 border border-gold">
-                    <img src="{{ asset('images/' . $produitB->image) }}" alt="{{ $produitB->nom }}" class="w-full h-48 object-cover rounded">
+                    <img src="{{ asset($produitB->image) }}" alt="{{ $produitB->nom }}" class="w-full h-48 object-cover rounded">
                     <h4 class="text-lg font-bold text-black mt-4">{{ $produitB->nom }}</h4>
                     <span class="block text-gold font-semibold mt-4">{{ number_format($produitB->prix, 2) }} €</span>
                     <a href="{{ route('produit.show', ['id' => $produitB->id]) }}" class="block mt-4 bg-black text-beige py-2 px-4 rounded text-center hover:bg-gold">Voir le produit</a>
@@ -38,7 +38,7 @@
             <!-- Produit 3 -->
             @if ($produitC)
                 <div class="bg-white shadow-md rounded-lg p-4 border border-gold">
-                    <img src="{{ asset('images/' . $produitC->image) }}" alt="{{ $produitC->nom }}" class="w-full h-48 object-cover rounded">
+                    <img src="{{ asset($produitC->image) }}" alt="{{ $produitC->nom }}" class="w-full h-48 object-cover rounded">
                     <h4 class="text-lg font-bold text-black mt-4">{{ $produitC->nom }}</h4>
                     <span class="block text-gold font-semibold mt-4">{{ number_format($produitC->prix, 2) }} €</span>
                     <a href="{{ route('produit.show', ['id' => $produitC->id]) }}" class="block mt-4 bg-black text-beige py-2 px-4 rounded text-center hover:bg-gold">Voir le produit</a>
